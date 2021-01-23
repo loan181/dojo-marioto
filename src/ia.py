251a8a2dd0795be0f134a3872d9cf174408166fc
Loan <loan181@hotmail.fr>
2021-01-23 14:35:37 +0100

    very basic ai

--- src/ia.py
+++ src/ia.py
@@ -1,3 +1,10 @@
 
 def step(game):
-    game.playerHighJump()
+    # Si c'est libre devant (ou l'objectif) et qu'il y a du sol en dessous
+    if game.getTilesAt(1, 0) in (" ", "G") and game.getTilesAt(1, -1) == "X":
+        game.playerMoveForward()
+    # S'il y a un trou suivi d'un bloc
+    elif game.getTilesAt(1, 0) == " " and game.getTilesAt(1, -1) == " " and game.getTilesAt(2, -1) == "X":
+        game.playerJump()
+    else:
+        game.playerLongJump()
